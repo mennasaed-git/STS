30dcee4515e48d2601ba9f661b276e43baf3af8c
menna saied <m.saied@ALMOSTAKBAL.local>
2020-09-03 03:43:29 -0700

    finall

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { GetSubcategoriesComponent } from './get-subcategories/get-subcategories
 import { GetProductsComponent } from './get-products/get-products.component';
 
 const routes: Routes = [
-  
+  {path:'' , component:GetCategoriesComponent  },
   {path:'Categories' , component:GetCategoriesComponent  },
   {path:'SubCategories/:CategoryId' , component:GetSubcategoriesComponent  },
   {path:'Products/:SubCategoryId' , component:GetProductsComponent  }
